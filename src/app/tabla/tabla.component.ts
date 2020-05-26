2dea8c2dea092a0f89e3cb78fc53a1d0d0b56f7d
gastonrodriguezz <lpmgaston@gmail.com>
2020-05-26 18:34:28 -0300

    primero

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -8,67 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent implements OnInit {
-  a = this.myTable();
-  constructor() {
+  sf: Array<number> = [];
+  pa: Array<number> = [];
+  st: Array<number> = [];
+  totalTotal = 0; totalMes = []; totalSf = 0;
+  totalPa = 0; totalSt = 0;
+  months: Array<string> = [];
+constructor() {
+  const sf = this.add12RandomNumber(this.sf);
+  const pa = this.add12RandomNumber(this.pa);
+  const st = this.add12RandomNumber(this.st);
+  const months = ['Enero',
+                  'Febrero',
+                  'Marzo',
+                  'Abril',
+                  'Mayo',
+                  'Junio',
+                  'Julio',
+                  'Agosto',
+                  'Septiembre',
+                  'Octubre',
+                  'Noviembre',
+                  'Diciembre'];
+  for (let i = 0; i < months.length; i++) {
+    this.totalMes[i] = sf[i] + pa[i] + st[i];
+    this.totalSf  = this.totalSf + sf[i];
+    this.totalPa = this.totalPa + pa[i];
+    this.totalSt = this.totalSt + st[i];
+    this.totalTotal = this.totalTotal + this.totalMes [i];
   }
-  ngOnInit(): void {
+  }
+ngOnInit(): void {
   }
   // numero random del 0 al 500
-  randomNumber() {
+randomNumber() {
     return Math.floor(Math.random() * 501);
     }
   // añade 12 elemtentos random a un array
-  add12RandomNumber(a: number[]) {
+add12RandomNumber(a: number[]) {
     for (let i = 0; i < 12; i++) {
       a.push(this.randomNumber());
     }
     return a;
   }
-  // crear tabla
-  myTable() {
-  let sf: Array<number> = [];
-  sf = this.add12RandomNumber(sf);
-  let pa: Array<number> = [];
-  pa = this.add12RandomNumber(pa);
-  let st: Array<number> = [];
-  st = this.add12RandomNumber(st);
-  let totalTotal = 0; let totalMes = 0; let totalSf = 0;
-  let totalPa = 0; let totalSt = 0;
-  const months: string[] = ['Enero',
-                            'Febrero',
-                            'Marzo',
-                            'Abril',
-                            'Mayo',
-                            'Junio',
-                            'Julio',
-                            'Agosto',
-                            'Septiembre',
-                            'Octubre',
-                            'Noviembre',
-                            'Diciembre'];
-  let myTable = '<table><thead><tr><th>Mes</th>';
-  myTable += '<th>Santa Fe</th>';
-  myTable += '<th>Paraná</th>';
-  myTable += '<th>Santo Tomé</th>';
-  myTable += '<th>Total</th></tr></thead><tbody>';
-  for (let i = 0; i < months.length; i++) {
-        totalMes = sf[i] + pa[i] + st[i];
-        myTable += '<tr><td>' + months[i] + '</td>';
-        myTable += '<td>' + sf[i] + '</td>';
-        myTable += '<td>' + pa[i] + '</td>';
-        myTable += '<td>' + st[i] + '</td>';
-        myTable += '<td>' + totalMes + '</td></tr>';
-        totalSf = totalSf + sf[i];
-        totalPa = totalPa + pa[i];
-        totalSt = totalSt + st[i];
-        totalTotal = totalTotal + totalMes;
-    }
-  myTable += '<tr><td style=\'font-size:18px;font-weight:lighter;\'>Total</td>';
-  myTable += '<td>' + totalSf + '</td>';
-  myTable += '<td>' + totalPa + '</td>';
-  myTable += '<td>' + totalSt + '</td>';
-  myTable += '<td>' + totalTotal + '</td></tr></tbody>';
-  myTable += '</table>';
-  return myTable;
-}
+
 }
