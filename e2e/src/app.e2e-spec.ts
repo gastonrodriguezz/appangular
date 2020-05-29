a5c740207defbbb9e78b7df4e9f888daed57703a
gastonrodriguezz <lpmgaston@gmail.com>
2020-05-29 18:29:37 -0300

    Nombre de proyecto cambiado

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('appangular2 app is running!');
+    expect(page.getTitleText()).toEqual('appangular app is running!');
   });
 
   afterEach(async () => {
